b33f98d0b417cfea33bb83eeb67f96f3c1509c18
gabriel moggi <gabimoggi30@gmail.com>
2024-09-27 18:50:30 -0300

    Estructura HTML y JS

---
+++ js/index.js
@@ -0,0 +1,43 @@
+//Calculadora de Prestamo
+let nombre=prompt("Ingresa tu nombre")
+if(nombre !==""){
+}else{
+    console.log("Reingresa tu nombre")
+}
+let apellido=prompt("Ingresa tu apellido")
+if(nombre !==""){
+}else{
+    console.log("Reingresa tu apellido")
+}
+let email=prompt("Ingresa tu email")
+if(nombre !==""){
+}else{
+    console.log("Reingresa tu email")
+}
+
+let montoInicial=parseFloat(prompt("Ingresa el prestamo a sacar"))
+
+function calcular(montoInicial) {
+    const TASA_INTERES = 0.25; 
+
+    function calcularInteres() {
+        return montoInicial * TASA_INTERES;
+    }
+
+    let intereses = calcularInteres(); 
+    console.log("Total de intereses: " + intereses);
+
+    let monto = montoInicial + intereses; 
+    console.log("Monto total a pagar: " + monto);
+
+    let años = prompt("Ingrese los años a pagar");
+    let meses = años * 12; 
+    let porMes = monto / meses; 
+
+    console.log("Pago por mes: " + porMes .toFixed(2));
+}
+calcular(montoInicial); 
+
+
+
+
