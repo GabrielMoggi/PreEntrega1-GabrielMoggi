2cfd14b1027895680c287b570b6889119bb56076
gabriel moggi <gabimoggi30@gmail.com>
2024-10-19 09:09:41 -0300

    Js modificado

--- js/index.js
+++ js/index.js
@@ -1,50 +1,52 @@
-//Calculadora de Prestamo
-let nombre=prompt("Ingresa tu nombre")
-if(nombre !==""){
-}else{
-    console.log("Reingresa tu nombre")
-}
-let apellido=prompt("Ingresa tu apellido")
-if(nombre !==""){
-}else{
-    console.log("Reingresa tu apellido")
-}
-let email=prompt("Ingresa tu email")
-if(nombre !==""){
-}else{
-    console.log("Reingresa tu email")
-}
-
-let montoInicial;
-while (true) {
-    montoInicial = parseFloat(prompt("Ingresa el préstamo a sacar"));
-    if (montoInicial > 0) {
-        break; 
-    }
-    alert("Por favor, ingresa un monto válido mayor que 0.");
-}
-function calcular(montoInicial) {
-    const TASA_INTERES = 0.25; 
-
-    function calcularInteres() {
-        return montoInicial * TASA_INTERES;
+class Prestamo {
+    constructor(nombre, apellido, email, montoInicial) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.montoInicial = montoInicial;
+        this.TASA_INTERES = 0.25;
+        this.intereses = this.calcularInteres();
+        this.montoTotal = this.calcularMontoTotal();
     }
 
-    let intereses = calcularInteres(); 
-    console.log("Total de intereses: " + intereses);
+    calcularInteres() {
+        return this.montoInicial * this.TASA_INTERES;
+    }
 
-    let monto = montoInicial + intereses; 
-    console.log("Monto total a pagar: " + monto);
+    calcularMontoTotal() {
+        return this.montoInicial + this.intereses;
+    }
 
-    let años = prompt("Ingrese los años a pagar");
-    let meses = años * 12; 
-    let porMes = monto / meses; 
-
-    console.log("Pago por mes: " + porMes .toFixed(2));
-    
+    calcularPagoMensual(años) {
+        let meses = años * 12;
+        return this.montoTotal / meses;
+    }
 }
-calcular(montoInicial); 
 
+let prestamos = [];
 
+function solicitarPrestamo() {
+    let nombre = prompt("Ingresa tu nombre");
+    let apellido = prompt("Ingresa tu apellido");
+    let email = prompt("Ingresa tu email");
 
+    let montoInicial;
+    while (true) {
+        montoInicial = parseFloat(prompt("Ingresa el préstamo a sacar"));
+        if (montoInicial > 0) {
+            break; 
+        }
+        alert("Por favor, ingresa un monto válido mayor que 0.");
+    }
 
+    const nuevoPrestamo = new Prestamo(nombre, apellido, email, montoInicial);
+    prestamos.push(nuevoPrestamo);
+
+    let años = parseInt(prompt("Ingrese los años a pagar"));
+    let pagoMensual = nuevoPrestamo.calcularPagoMensual(años);
+
+    console.log(`Préstamo de ${nuevoPrestamo.montoInicial} por ${años} años`);
+    console.log("Total de intereses: " + nuevoPrestamo.intereses);
+    console.log("Monto total a pagar: " + nuevoPrestamo.montoTotal);
+    console.log("Pago por mes: " + pagoMensual.toFixed(2));
+}
